831ff665764583ecfd58798a465fcf10e71ebce6
Mybui11qwer <mybui11qwer@gmail.com>
2025-07-15 09:59:20 +0700

    update

--- appdungcucaulong/lib/core/network/api_constants.dart
+++ appdungcucaulong/lib/core/network/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const baseUrl = 'http://10.21.13.141:3000/api';
+  static const baseUrl = 'http://10.13.21.138:3000/api';
   //auth
   static const login = '$baseUrl/login';
   static const register = '$baseUrl/register';
