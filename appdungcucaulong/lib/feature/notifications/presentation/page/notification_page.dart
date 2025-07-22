62a04689a4a5dec1bc3ac88a08de4d833f026593
bladeonfire2402 <liemp966@gmail.com>
2025-07-22 17:55:23 +0700

    new

---
+++ appdungcucaulong/lib/feature/notifications/presentation/page/notification_page.dart
@@ -0,0 +1,18 @@
+import 'package:appdungcucaulong/config/shared/widget/main_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MainScaffold(
+      currentIndex: 1,
+      body: Column(
+        children: [
+          Text('ddddd')
+        ],
+      ),
+    );
+  }
+}
